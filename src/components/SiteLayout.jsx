eb54b899cdac81d73dd6586e79bd8e98cf4ccf0d
Mohammad Saleh <abbasisaleh895@gmail.com>
2026-09-21 22:42:56 +0430

    Fix search params build with suspense

--- src/components/SiteLayout.jsx
+++ src/components/SiteLayout.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Suspense } from "react";
 import { usePathname } from "next/navigation";
 import Header from "./Header";
 import Footer from "./Footer";
@@ -16,7 +17,9 @@ export default function SiteLayout({ children }) {
   return (
     <div className="flex min-h-screen flex-col">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <Header />
+        <Suspense fallback={null}>
+          <Header />
+        </Suspense>
 
         <main>{children}</main>
       </div>
